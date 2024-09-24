21a3d9a7b59c355e6af730eff063a23c588b16ab
sadman-zakaria <sadicon5152@gmail.com>
2024-09-24 19:29:11 +0600

    Assignment-2

---
+++ prob1/problem0.1.js
@@ -0,0 +1,81 @@
+//1.
+ let carName = "Volvo";
+   console.log(carName);
+
+
+
+//2.
+let firstName = "John", lastName = "Doe",age = 35;
+
+
+
+//3.
+let x = 10;
+  let y = 5;
+  x = x*y ;
+  console.log(x); //50
+
+  
+
+
+
+//4.
+let length = 16;          //data type: Number 
+  let lastName = "Johnson";  //data type: string 
+
+   const x = {
+     firstName: "John",  
+      lastName: "Doe"
+   };     //data type: object
+
+
+
+//5.
+  function myFunction() {
+  alert("Hello World!");
+  }
+  myFunction(); 
+
+
+
+//6.
+ let person ={personName : "Johon",age :50 };  
+
+alert(person.personName + "is" + person.age)
+
+
+//7.
+function myFunctionn(){
+  alert("Button was clicked!")} 
+
+
+//8.  
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert( cars.length);
+
+
+//9.
+
+//1.
+let randomNumber = Math.random();
+alert(randomNumber);
+
+//2.
+let largestNumber = Math.max(10, 20);
+alert(largestNumber);
+
+//3.
+let squareRoot = Math.sqrt(9)
+alert(squareRoot);  
+
+
+
+
+//10.
+
+let xp = 10;
+let yp = 5;
+alert(xp > yp);
+
+
+  
---
+++ prob2/problem0.2.js
@@ -0,0 +1,72 @@
+//1.
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+  }
+ 
+  console.log(celsiusToFahrenheit(0));  //32
+  console.log(celsiusToFahrenheit(25)); // 77
+
+
+
+ //2. 
+ function isEven(num) {
+    return num % 2 === 0;
+  }
+ 
+  console.log(isEven(4));  // true
+  console.log(isEven(7));  // false
+
+
+  //3.
+  function sum(a,b){
+    return a + b;
+}
+console.log(sum(3, 4)); // 7
+console.log(sum(10, 20)); // 30
+
+
+
+//4.
+
+
+
+  //5.
+
+
+
+//6.  
+function getFirstElement(arr) {
+    return arr[0];
+  }
+ 
+  console.log(getFirstElement([1, 2, 3])); // 1
+  console.log(getFirstElement(["a", "b", "c"])); // "a"
+
+
+
+
+//7.
+function isArrayEmpty(arr63) {
+    return arr63.length === 0;
+  }
+ 
+  console.log(isArrayEmpty([])); // true
+  console.log(isArrayEmpty([1, 2, 3])); // false
+
+
+//8.  
+
+
+
+
+
+//9.
+function reverseString(llpd1) {
+    return llpd1.split(``).reverse().join(``);
+  }
+ 
+  console.log(reverseString("hello")); // "olleh"
+  console.log(reverseString("world")); // "dlrow"
+
+
+  
